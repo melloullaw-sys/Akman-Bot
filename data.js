c53827e980663442584b73296e1468cfb8d3864b
melloullaw-sys <melloullaw@gmail.com>
2026-03-03 01:15:54 +0200

    Move translation and portfolio data to data.js

--- data.js
+++ data.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// translations
+export const translations = {
+    // ... your translation data here
+};
 
-module.exports = nextConfig
+// portfolioData
+export const portfolioData = [
+    // ... your portfolio data here
+];
+
+// aiPicksData
+export const aiPicksData = [
+    // ... your AI picks data here
+];
+
+// supercomputingData
+export const supercomputingData = [
+    // ... your supercomputing data here
+];
